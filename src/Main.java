4fa7e790c85cd5c27bb28af3a2f2c62febbe0a10
Gideonkamaumacharia <kamaugedion95@gmail.com>
2025-12-18 08:02:25 +0300

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+//        int[] numbers = {1,4,2,7,19};
+       PracticeTest test = new PracticeTest();
+//        System.out.println(test.largestNum(numbers));
+        String string = new String("Veronica");
+
+        System.out.println(test.reverseString(string));
+
+        }
+
+    }
